a0444f307877fbbdefd3266d2ededded1e067c2d
Billy.Zheng <zw963@163.com>
2017-07-31 00:36:27 +0800

    Add a failed promise example.

---
+++ spec/async_spec.rb
@@ -0,0 +1,12 @@
+describe 'async' do
+  subject do
+    promise = Promise.new
+    delay 1 do
+      promise.resolve 42
+    end
+    promise
+  end
+
+  it { is_expected.to be_a Promise }
+  it { is_expected.to eq 42 }
+end
