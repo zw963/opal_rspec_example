1d6f1e5dc4f7cdb2fa4bf71d605ee5fc975bdd2d
Billy.Zheng <zw963@163.com>
2017-08-17 23:35:05 +0800

    添加另一个测试 method 的测试.

--- spec/test_one_class_spec.rb
+++ spec/test_one_class_spec.rb
@@ -1,10 +1,15 @@
 require 'test_one_class'
 require 'native'
+require 'date'
 
 describe 'JsClassToBeTested' do
   let(:user) { Native(`new JsClassToBeTested('bill', 'thompson')`) }
 
   it 'shuld be able to test an attribute' do
     user.fullName.should == 'bill thompson'
+  end
+
+  it 'should be able to test a function' do
+    user.yearBornIfThisOld(10).should == Date.today.year - 10
   end
 end
