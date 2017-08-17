4b06dbbda272d44a2ce4593317ab8bc807fbe4ea
Billy.Zheng <zw963@163.com>
2017-08-17 23:31:41 +0800

    添加了第一个测试属性的测试.

--- spec/test_one_class_spec.rb
+++ spec/test_one_class_spec.rb
@@ -1,1 +1,10 @@
 require 'test_one_class'
+require 'native'
+
+describe 'JsClassToBeTested' do
+  let(:user) { Native(`new JsClassToBeTested('bill', 'thompson')`) }
+
+  it 'shuld be able to test an attribute' do
+    user.fullName.should == 'bill thompson'
+  end
+end
