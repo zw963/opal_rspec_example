4b4e5c4c0b45e0f30a6e54b9d94ffcbcd9b36ce7
Billy.Zheng <zw963@163.com>
2017-08-18 22:42:51 +0800

    rename dom_test.rb to dom_spec.rb

---
+++ spec/dom_spec.rb
@@ -0,0 +1,27 @@
+# require 'jquery-1.12.4.min'
+# require 'jquery-migrate-1.4.1.min'
+require 'zepto.min'
+require 'opal'
+require 'opal-jquery'
+
+# describe 'DOM Testing' do
+#   it 'should see dom changes' do
+#     div = <<-'HEREDOC'
+# <div id="target-div">
+#   original text
+# </div>
+# HEREDOC
+
+#     # # 我们可以使用 Element.parse 来建立这个一个 Dom 元素.
+#     # div = Element.parse(div)
+#     # # puts div.methods
+#     # puts '*'*100
+#     # div.append_to_body
+
+#     # d = Element.find('#target-div')
+#     # expect(d.text).to =~ /original test/
+
+#     # `document.getElementById('target-div').innerText = 'js replaced text'`
+#     # expect(d.text).to =~ /js replaced text/
+#   end
+# end
